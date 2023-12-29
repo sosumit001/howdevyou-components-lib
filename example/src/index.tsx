ec53b0a5ac31cdbc3ceb7a13dcbf6a62546f26b2
sosumit001 <sosumit001@gmail.com>
2023-12-30 03:42:07 +0530

    ../

---
+++ example/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import { MyCounter } from 'my-react-typescript-package'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <div>we gonna import text here</div>
+  </React.StrictMode>,
+)
